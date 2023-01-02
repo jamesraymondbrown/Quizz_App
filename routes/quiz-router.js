70f7547826de80d57c8fd2bb29f89216cc53b150
Nikko Bansil <bjohnnikko@gmail.com>
2023-01-02 14:19:38 -0800

    / route points to the quiz EJS

--- routes/quiz-router.js
+++ routes/quiz-router.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 // GET /quiz
-router.get('', (req, res) => {
-
+router.get('/', (req, res) => {
+  res.render('quiz');
 });
 
 
