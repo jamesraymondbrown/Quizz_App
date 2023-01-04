eaca2a1c194a2a5096b54bd56810c5007543d33b
Nikko Bansil <bjohnnikko@gmail.com>
2023-01-03 17:47:25 -0800

    finish logic for saving quizzes

--- db/queries/quiz-queries.js
+++ db/queries/quiz-queries.js
@@ -7,6 +7,22 @@ const getQuizzes = () => {
     });
 };
 
+const addQuiz = (quiz) => {
+  return db.query('INSERT INTO quizzes (name, description) VALUES ($1, $2) RETURNING *;', [quiz.name, quiz.description])
+    .then((response) => {
+      return response.rows[0];
+    });
+};
+
+const addQuestion = (question) => {
+  return db.query('INSERT INTO questions (question, correct_answer, option1, option2, option3, quiz_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;', [question.name, question.correct, question.option1, question.option2, question.option3, question.quizId])
+    .then((response) => {
+      return response.rows[0];
+    });
+};
+
 module.exports = {
-  getQuizzes
+  getQuizzes,
+  addQuiz,
+  addQuestion
 };
--- routes/quiz-router.js
+++ routes/quiz-router.js
@@ -16,7 +16,29 @@ router.get('/new', (req, res) => {
 
 // POST /quiz/new
 router.post('/new', (req, res) => {
-  res.render('new-quiz');
+  const quizData = {
+    name: req.body['quiz-name'],
+    description: req.body['quiz-name']
+  };
+  quizQueries.addQuiz(quizData)
+    .then((quiz) => {
+      return quiz.id;
+    })
+    .then((quiz_id) => {
+      for (let x = 1; x <= 10; x++) {
+        const questionData = {
+          name: req.body[`q${x}-description`],
+          correct: req.body[`q${x}-answer`],
+          option1: req.body[`q${x}-option1`],
+          option2: req.body[`q${x}-option2`],
+          option3: req.body[`q${x}-option3`],
+          quizId: quiz_id
+        };
+        quizQueries.addQuestion(questionData)
+      }
+    });
+
+    res.render('/');
 });
 
 module.exports = router;
