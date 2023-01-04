7d1f65fee3bd9bdc8da57bc59531016e6051ca5e
James Brown <jamesraymondbrown@gmail.com>
2023-01-04 23:45:50 +0000

    finish logout functionality, add default user to db

--- routes/account.js
+++ routes/account.js
@@ -7,7 +7,6 @@
 
 const express = require('express');
 const router  = express.Router();
-//const userQueries = require('../db/queries/users'); --> connect to the db?
 
 router.use((req, res, next) => {
   if (!req.session.userId) {
