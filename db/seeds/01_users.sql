f1323303fd212092e4cde59d1b993c9ef7d4ce2a
James Brown <jamesraymondbrown@gmail.com>
2023-01-04 18:56:09 +0000

    add bcrypt and cookieSession to dev dependencies

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,3 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, password)
+VALUES ('James Brown', 'james@brown.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
