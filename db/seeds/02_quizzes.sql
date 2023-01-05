b0a97502bbe27adf5b9a9765c411770e2f4cdbbd
Nikko Bansil <bjohnnikko@gmail.com>
2023-01-05 10:56:36 -0800

    add another quiz in the seed

--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -1,2 +1,4 @@
 -- Users table seeds here (Example)
 INSERT INTO quizzes (name, description, private, user_id) VALUES ('Star Wars Prequels', 'Star wars quiz', FALSE, 1);
+
+INSERT INTO quizzes (name, description, private, user_id) VALUES ('Harry Potter Series', 'A Harry Potter trivia quiz for muggle and non-muggle enthusiasts.', FALSE, 3);
