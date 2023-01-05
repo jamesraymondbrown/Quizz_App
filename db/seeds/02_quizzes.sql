442bc9eae0252f8316038f7eb76f9d51211f5575
larsyaeger <larsyaeger@gmail.com>
2023-01-05 12:49:11 -0600

    star wars quiz inserted

--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -1,11 +1,1 @@
--- Users table seeds here (Example)
-<<<<<<< HEAD
-INSERT INTO quizzes (name, description, private, user_id) VALUES ('Quiz One', 'FIRST QUIZ', True, 3);
-INSERT INTO quizzes (name, description, private, user_id) VALUES ('Star Wars Prequels', 'A quiz to test your knowledge of the superior Star Wars films.', False, 4);
-=======
-INSERT INTO quizzes (name, description) VALUES ('Quiz One', 'FIRST QUIZ');
-INSERT INTO quizzes (name, description) VALUES ('Star Wars Prequels', 'A quiz to test your knowledge of the superior Star Wars films.');
-INSERT INTO quizzes (name, description, type, private, user_id) VALUES ('Quiz One', 'FIRST QUIZ', 'multichoice', FALSE, 1);
-INSERT INTO quizzes (name, description, type, private, user_id) VALUES ('Quiz Two', 'SECOND QUIZ', 'multichoice', TRUE, 1);
-INSERT INTO quizzes (name, description, type, private, user_id) VALUES ('Quiz Three', 'Thrird QUIZ', 'multichoice', FALSE, 1);
->>>>>>> 08f5a066c4b7bff313eaf601091920f0985b8a57
+INSERT INTO quizzes (name, description, private, user_id) VALUES ('Star Wars Prequels', 'Star wars quiz', FALSE, 1);
