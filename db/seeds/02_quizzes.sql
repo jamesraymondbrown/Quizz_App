60cb0face278890de60c6c41b43330f2b4a8bb08
Nikko Bansil <bjohnnikko@gmail.com>
2023-01-04 19:44:44 -0800

    add quiz type and user id

--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -1,2 +1,2 @@
 -- Users table seeds here (Example)
-INSERT INTO quizzes (name, description) VALUES ('Quiz One', 'FIRST QUIZ');
+INSERT INTO quizzes (name, description, type, user_id) VALUES ('Quiz One', 'FIRST QUIZ', 'Public', 1);
