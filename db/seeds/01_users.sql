e577f0304d686916e049eea3185338e8cd3d66e9
James Brown <jamesraymondbrown@gmail.com>
2023-01-05 18:52:22 +0000

    add quiz to schema and seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -3,3 +3,7 @@ INSERT INTO users (name, email, password)
 VALUES ('James', 'james@james.com', '$2a$10$LGkmNfTLnlTQQbc1I9dc0usUlg4nzvU890xMV2Vhy.V7n0rfCHwxO');
 INSERT INTO users (name, email, password)
 VALUES ('James Brown', 'jamesbrown@browntown.com', '$2a$10$8k2aceF/gqDILYII0HOy/emDoQxWTMg6msnhCeByGfcQTcu95qULO');
+INSERT INTO users (name, email, password)
+VALUES ('Nikko Bansil', 'nikko@test.com', '$2a$10$iZzvh7cV1CT20SHsmLkUuuoc7Y9D.l3DQho.rndLPC7E2/AdU2T/2');
+INSERT INTO users (name, email, password)
+VALUES ('Lars', 'lars@lars.lars', '$2a$10$La2/4GTdxEiqeg59jN07h..mRSVZ6kLADvmQ3IP2jnsmLcKlZGMAS');
--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -1,6 +1,4 @@
 -- Users table seeds here (Example)
-INSERT INTO quizzes (name, description) VALUES ('Quiz One', 'FIRST QUIZ');
-INSERT INTO quizzes (name, description) VALUES ('Star Wars Prequels', 'A quiz to test your knowledge of the superior Star Wars films.');
-INSERT INTO quizzes (name, description, type, private, user_id) VALUES ('Quiz One', 'FIRST QUIZ', 'Public', 1);
-INSERT INTO quizzes (name, description, type, private, user_id) VALUES ('Quiz Two', 'SECOND QUIZ', 'Private', 1);
-INSERT INTO quizzes (name, description, type, private, user_id) VALUES ('Quiz Three', 'Thrird QUIZ', 'Public', 1);
+INSERT INTO quizzes (name, description, private, user_id) VALUES ('Star Wars Prequels', 'A quiz to test your knowledge of the superior Star Wars films.', FALSE, 4);
+INSERT INTO quizzes (name, description, private, user_id) VALUES ('Do you know who The Beatles are?', 'A quiz to find out if you know who The Beatles are', FALSE, 1);
+
