f951ee645dfd8c3691d9ef2ff829991f1665a175
larsyaeger <larsyaeger@gmail.com>
2023-01-04 21:27:18 -0600

    add star wars quiz and description

--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -1,2 +1,3 @@
 -- Users table seeds here (Example)
 INSERT INTO quizzes (name, description) VALUES ('Quiz One', 'FIRST QUIZ');
+INSERT INTO quizzes (name, description) VALUES ('Star Wars Prequels', 'A quiz to test your knowledge of the superior Star Wars films.');
